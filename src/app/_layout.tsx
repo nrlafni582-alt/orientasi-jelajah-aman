629d2575d058b133e1a1f1b5625ba1837d46acbc
Nurul Afni Khasifa <sifakhasifa91@gmail.com>
2026-09-22 10:09:55 +0700

    feat: tambah modal tambah favorit

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,18 +1,15 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useColorScheme } from 'react-native';
+// app/_layout.tsx (root layout)
+import { Stack } from "expo-router";
 
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
-
-SplashScreen.preventAutoHideAsync();
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
+export default function RootLayout() {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="detail/[kota]" options={{ title: "Detail Cuaca" }} />
+      <Stack.Screen
+        name="tambah-favorit"
+        options={{ presentation: "modal", title: "Tambah Favorit" }}
+      />
+    </Stack>
   );
 }
