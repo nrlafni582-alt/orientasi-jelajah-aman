c3cd9ab152bdb1b862cb1eee161aed854a068a5d
Nurul Afni Khasifa <sifakhasifa91@gmail.com>
2026-09-23 11:27:47 +0700

    fix: lengkapi tombol tambah favorit yang tertinggal di Pertemuan 3

--- src/app/detail/[kota].tsx
+++ src/app/detail/[kota].tsx
@@ -1,14 +1,18 @@
-// app/detail/[kota].tsx
-import { useLocalSearchParams } from "expo-router";
-import { View } from "react-native";
+// src/app/detail/[kota].tsx
+import { router, useLocalSearchParams } from "expo-router";
+import { Button, View } from "react-native";
 import WeatherCard from "../../components/WeatherCard";
 
 export default function HalamanDetail() {
   const { kota } = useLocalSearchParams<{ kota: string }>();
 
   return (
-    <View style={{ padding: 16 }}>
+    <View style={{ padding: 16, gap: 16 }}>
       <WeatherCard kota={kota} suhu={29} tingkatAQI="BAIK" />
+      <Button
+        title="Tambahkan ke Favorit"
+        onPress={() => router.push("/tambah-favorit")}
+      />
     </View>
   );
 }
